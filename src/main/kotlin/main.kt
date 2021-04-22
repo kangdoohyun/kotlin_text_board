6609e11f76816cd412e491e3f37b8681e791204d
kangdoohyun <rkdengus1208@gmail.com>
2021-04-22 10:50:51 +0900

    article delete 기능 구현

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,61 +1,87 @@
-
 import java.text.SimpleDateFormat
 
-
 fun readLineTrim() = readLine()!!.trim()
-
 fun main() {
-    println("== 게시판 관리 프로그램 시작 ==")
-
-    var articlesLastId = 0
+    println("게시물 관리 프로그램 실행")
+    var articleLastId = 0
     val articles = mutableListOf<Article>()
 
-    loop@while(true){
-        print("명령어 입력 : ")
-        val command = readLineTrim()
-
-        when (command){
-            "system exit" -> {
-                println("시스템을 종료합니다")
-                break@loop
+    loop@while (true){
+        print("명령어 : ")
+        val commend = readLineTrim()
+        when{
+            commend == "system exit" -> {
+                println("프로그램을 종료합니다")
+                break
             }
-            "article write" -> {
-                val id = articlesLastId +1
+            commend.startsWith("article delete") -> {
+                val id = commend.trim().split(" ")[2].toInt()
+                var articleToDelete: Article? = null
+                for (article in articles) {
+                    if (article.id == id) {
+                        articleToDelete = article
+                    }
+                }
+                if (articleToDelete == null) {
+                    println("${id}번 게시물은 존재하지 않습니다")
+                    continue@loop
+                }
+                articles.remove(articleToDelete)
+                println("${id}번 게시물을 삭제하였습니다")
+
+            }
+            commend ==  "article write" -> {
+                val id = articleLastId +1
                 val regDate = Util.getNowDateStr()
                 val updateDate = Util.getNowDateStr()
-
                 print("제목 : ")
                 val title = readLineTrim()
-
                 print("내용 : ")
                 val body = readLineTrim()
 
-                var article = Article(id, regDate, updateDate, title, body)
+                val article = Article(id, regDate, updateDate, title, body)
+
                 articles.add(article)
 
-                println("${id}번 게시물이 작성되었습니다")
-
-                articlesLastId = id
+                articleLastId = id
             }
-            "article list" -> {
-                println("번호  /  제목  /  작성일  /  수정일")
+            commend == "article list" -> {
+                println("번호  /  작성일  /  수정일  /  제목")
                 for (article in articles){
-                    println("${article.id}  /  ${article.title}  /  ${article.regDate}  /  ${article.updateDate}")
+                    println("${article.id}  /  ${article.regDate}  /  ${article.updateDate}  /  ${article.title}")
                 }
             }
             else -> {
-                println("$command 는 존재하지 않는 명령어 입니다")
+                println("$commend 는 존재하지 않는 명령어입니다")
             }
         }
     }
-    println("== 게시판 관리 프로그램 종료 ==")
+    println("게시물 관리 프로그램 종료")
 }
-data class Article(val id : Int, val regDate : String, val updateDate : String, val title : String, val body : String){
+data class Article(val id : Int, val regDate: String, val updateDate: String, val title: String, val body: String){
 
 }
 object Util{
     fun getNowDateStr(): String {
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-        return dateFormat.format(System.currentTimeMillis())
+        val format1 = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        return format1.format(System.currentTimeMillis())
+    }
+
+}
+fun bubbleSort(numbers: MutableList<Int>) {
+    val lastIndex = numbers.size -1
+    var depth = lastIndex
+
+    loop1@while (depth >= 1 ){
+        for (i in 0 until depth){
+            if(i == depth-1 && numbers[i] < numbers[i+1]){
+                depth-=2
+                continue@loop1
+            }
+            if(numbers[i] > numbers[i+1]){
+                numbers[i] = numbers[i+1].also { numbers[i+1] = numbers[i] }
+            }
+        }
+        depth--
     }
 }
