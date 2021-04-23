d4f000a7ba5a1a6cebd55148854522e8d014bf42
kangdoohyun <rkdengus1208@gmail.com>
2021-04-23 19:52:09 +0900

    게시물 검색기능 구현

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,4 @@
 import java.text.SimpleDateFormat
-import kotlin.math.ceil
 
 fun main() {
     println("게시물 관리 프로그램 실행")
@@ -65,15 +64,33 @@ fun main() {
                 println("${id}번 게시물이 작성되었습니다")
             }
             command.startsWith("article list ") -> {
-                val currentPage = command.split(" ")[2].toInt()
-                val itemsInAPage = 10
-                val jumpIndex = (currentPage - 1) * itemsInAPage
+                // command를 나눈다 [article, list, 검색어, 페이지번호]
+                val commandBits = command.trim().split(" ")
+                // page랑 searchKeyword를 임시값으로 초기화 한다
+                var page = 1
+                var searchKeyword = ""
+                // 내가 command를 article list 제목_1 1 로 입력해서 검색어가 있다면 commandBits.size가 4이라 if가 실행
+                // 내가 command를 article list 1 로 입력해서 검색어가 없다면 commandBits.size가 3이라 else if가 실행
+                if (commandBits.size == 4) {
+                    // searchKeyword 가 commandBits [article, list, 검색어, 페이지번호] 의 2번째 인덱스 즉 '검색어' 가 된다
+                    searchKeyword = commandBits[2]
+                    // page가 commandBits [article, list, 검색어, 페이지번호] 의 3번째 인덱스 즉 '페이지번호' 가 된다
+                    page = commandBits[3].toInt()
+                } else if (commandBits.size == 3) {
+                    // commandBits가 3이면 검색어는 없기떄문에 페이지 번호가 2번째 인덱스가 된다
+                    page = commandBits[2].toInt()
+                }
+                // 한페이지에 몇개의 개시물을 출력할지 정하는 변수
+                val itemsCountInAPage = 10
+                // 페이지가 몇이냐에 따라 몇개의 개시물을 건너뛰고 출력해야 할지 넘겨주는 변수
+                val offsetCount = (page - 1) * itemsCountInAPage
+                // getFilteredArticles 함수 실행
+                val filteredArticles = getFilteredArticles(searchKeyword, offsetCount, itemsCountInAPage)
 
-                val articles = getFilteredArticles(jumpIndex, itemsInAPage)
-
-                println("번호 /         작성일         /         수정일         /  제목")
-                for (article in articles){
-                    println("${article.id}  /  ${article.regDate}  /  ${article.updateDate}  /  ${article.title}")
+                println("번호 / 작성날짜 / 제목")
+                // getFilteredArticles 함수에서 리턴된 filtered2Articles로 반복문을 돌려서 출력
+                for (article in filteredArticles) {
+                    println("${article.id} / ${article.regDate} / ${article.title}")
                 }
             }
             else -> {
@@ -91,15 +108,38 @@ var articlesLastId = 0
 
 val articles = mutableListOf<Article>()
 
-fun getFilteredArticles(jumpIndex: Int, itemsInAPage: Int): MutableList<Article> {
-    val startIndex = articles.lastIndex - jumpIndex
-    val endIndex = startIndex - itemsInAPage + 1
+fun getFilteredArticles(searchKeyword: String, jumpIndex: Int, itemsInAPage: Int): MutableList<Article> {
+    // articles 를 filtered1Articles 에 저장해 검색어가 없는경우에 모든 게시물을 상대로 리스팅할수있게 대비
+    var filtered1Articles = articles
+    // if문으로 searchKeyword 가 비어있는지 비어있지 않은지 검증 isNotEmpty 이기때문에 비어있지 않으면 true 리턴
+    if (searchKeyword.isNotEmpty()) {
+        // if문이 true 이면 전체개시물들을 저장해둔 filtered1Articles 에 비어있는 mutableList 를 새로 정의
+        filtered1Articles = mutableListOf<Article>()
 
-    val filteredArticles = mutableListOf<Article>()
-    for (i in startIndex downTo endIndex){
-        filteredArticles.add(articles[i])
+        for (article in articles) {
+            // articles 에서 반복문으로 한개씩 끌어온 article 의 title 이랑 내가 입력한 검색어 searchKeyword가 포합되어있으면 true
+            if (article.title.contains(searchKeyword)) {
+                // 내가 검색한 검색어가 포함된 article을 걸러냈으니 걸러낸 결과물 즉 검색결과를 filtered1Articles에 add해서 추가
+                filtered1Articles.add(article)
+            }
+        }
     }
-    return filteredArticles
+    // 리스팅할 새로운 mutableList를정의
+    val filtered2Articles = mutableListOf<Article>()
+
+    val startIndex = filtered1Articles.lastIndex - jumpIndex
+    var endIndex = startIndex - itemsInAPage + 1
+
+    if (endIndex < 0) {
+        endIndex = 0
+    }
+    // 시작 인덱스 부터 역순downTO 로 한개씩 감소시키며 종료 인덱스까지 인덱스를 하나씩 i 에 담아서 반복문 실행
+    for (i in startIndex downTo endIndex) {
+        // 위에서 새로 정의한 리스팅할 mutableList인 filtered2Articles에 위에서 검색어를 포함한 article을 add한 filtered1Articles를 add
+        filtered2Articles.add(filtered1Articles[i])
+    }
+
+    return filtered2Articles
 }
 
 fun getArticleById(id: Int): Article? {
